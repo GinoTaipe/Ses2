2f138010fc78830e0acb88f47fef048960cf03a0
Gino <gino_gjtm@hotmail.com>
2019-08-01 22:42:44 -0500

    Primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+//Ejecutar una vez cargada todo el HTML
+window.onload=function(){
+    let nombre=document.getElementById("nombre"); //Obtienes el objeto
+    let btn=document.getElementById("btnEnviar");
+    btn.addEventListener("click",saludo);
+
+    function saludo(){
+        alert('Hola '+nombre.value);
+    }
+}
